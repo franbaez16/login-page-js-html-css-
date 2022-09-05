15a6f5d6d3fef38c5fceb9694fb8c9ea2f4037be
Fran Baez <103434629+franbaez16@users.noreply.github.com>
2022-09-05 12:48:04 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+function logearse(){
+    var x,y;
+    x = document.getElementById("usuario").value; 
+    y = document.getElementById("contra").value;
+    console.log(x)
+    document.getElementById("resultado").innerHTML = text
+    var usuario="eddie34"
+    var contra="eddie.forever"
+}
+if (usuario== x && contra== y){
+ 
+    else(usuario)
+}
